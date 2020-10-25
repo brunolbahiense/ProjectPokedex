3a07201c0485ff2d8d3771b6ade661cb5f003b50
BrunolBahiense <brunoluisbahiense@gmail.com>
2020-10-25 15:59:33 -0300

    estrutura inicial do projeto

---
+++ pokedex-master/app.js
@@ -0,0 +1,31 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const generatePokemonPromisses = () => Array(150).fill().map((_, index) =>
+     fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+
+const generateHtml = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
+    const elementTypes = types.map(typeInfo => typeInfo.type.name)
+
+    accumulator += `
+        <li class="card ${elementTypes[0]}">
+        <img class="card-image" alt="${name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" />
+        <h2 class="card-title">${name}</h2>
+        
+        </li>`
+    return accumulator
+
+    }, '')
+
+
+
+const insertPokemonsIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+
+    ul.innerHTML = pokemons
+}
+
+
+const pokemonPromisses = generatePokemonPromisses()
+Promise.all(pokemonPromisses)
+    .then(generateHtml)
+    .then(insertPokemonsIntoPage)
