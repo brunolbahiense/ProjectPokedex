4ea9127b27d401463f236ec14578a45431c3340c
Brunolbahiense <brunoluisbahiense@gmail.com>
2022-04-27 20:39:50 -0300

    types added

--- PokedexMaster/app.js
+++ PokedexMaster/app.js
@@ -3,18 +3,17 @@ const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 const generatePokemonPromisses = () => Array(151).fill().map((_, index) =>
      fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-const generateHtml = pokemons => pokemons.reduce((accumulator, { name, id, types,  }) => {
+const generateHtml = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
     const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
     accumulator += `
         <button class="card ${elementTypes[0]}">
         <img class="card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" />
         <h2 class="card-title">${name}</h2>
-        
+        <p><span>${elementTypes[0]}</span>  <span>${elementTypes[1] === undefined ? '': '| ' + elementTypes[1]}</span></p>
         </button>`
     return accumulator
-
-    }, '')
+}, '')
 
 const insertPokemonsIntoPage = pokemons => {
     const ul = document.querySelector('[data-js="pokedex"]')
